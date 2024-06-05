73f009b1b1c0c41f864a550c1283f91192d3840a
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-06-06 04:48:25 +0530

    feat(auth): added auth

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Library from '../Library/Library'
 import Feed from '../Feed/Feed'
@@ -7,8 +7,29 @@ import Player from '../Player/Player'
 import Favourites from '../Favourites/Favourites'
 import './Home.css'
 import Sidebar from '../../components/Sidebar/Sidebar'
+import Login from '../auth/Login'
+import { setClientToken } from '../../spotify'
 const Home = () => {
-  return (
+
+  const [token, setToken] = useState("")
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token")
+    const hash = window.location.hash
+    window.location.hash = ""
+    if(!token && hash){
+      const _token = hash.split('&')[0].split("=")[1];
+      window.localStorage.setItem("token", _token)
+      setToken(_token)
+      setClientToken(_token)
+    } else {
+      setToken(token)
+      setClientToken(token)
+    }
+    
+  }, [])
+  return !token ? (
+    <Login/> ) : (
     <Router>
       <div className='main-body'>
         <Sidebar></Sidebar>
