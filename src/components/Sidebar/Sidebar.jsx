a4514211b36d146aaa52dbbc5a2e864515b0c459
Aryaman Chandrawat <singharyaman357@gmail.com>
2024-06-06 04:46:13 +0530

    feat: Sidebar updated

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Sidebar.css'
 import SidebarButton from './SidebarButton'
 import { MdSpaceDashboard } from 'react-icons/md'
@@ -6,11 +6,18 @@ import { MdFavorite } from 'react-icons/md'
 import { FaGripfire, FaPlay } from 'react-icons/fa6'
 import { FaSignOutAlt } from 'react-icons/fa'
 import { IoLibrary } from 'react-icons/io5'
+import apiClient from '../../spotify'
 
 const Sidebar = () => {
+  const [image, setImage] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS92eisuWOx3tEjeW14mT9ACVgXDwIRBGtnww&s")
+  useEffect(() => {
+    apiClient.get("me").then(response => {
+      setImage(response.data.images[0].url);
+    })
+  }, [])
   return (
     <div className='sidebar-container'>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS92eisuWOx3tEjeW14mT9ACVgXDwIRBGtnww&s" alt="profile" className='profile-img'/>
+      <img src={image} alt="profile" className='profile-img'/>
       <div>
         <SidebarButton title="Feed" to="/feed" icon={<MdSpaceDashboard/>}></SidebarButton>
         <SidebarButton title="Trending" to="/trending" icon={<FaGripfire/>}></SidebarButton>
